47bbd208318cb319c4d66af12b2810aac8b35632
eriveltonluiz <erivelton-paulista@hotmail.com>
2021-03-28 15:47:01 -0300

    Na janela flutuante dos materiais foi implementado um carrousel para ir vendo os mesmos, conforme vai clicando no próximo ou anterior, incluso com o cadastro desses materiais e uma pesquisa personalizada.

--- FrontEnd_Angular_Doacao-main/src/app/componentes/pai/material/list-materiais/list-materiais.component.ts
+++ FrontEnd_Angular_Doacao-main/src/app/componentes/pai/material/list-materiais/list-materiais.component.ts
@@ -7,38 +7,72 @@ import { Material } from 'src/app/model/material';
   styleUrls: ['./list-materiais.component.css']
 })
 export class ListMateriaisComponent implements OnInit {
-materiais: Array<Material> = [
-  {
-    id: 1,
-    descricao: 'apontador',
-    quantidadeDesejada: 2,
-    urlFoto: '/assets/imagens/apontador.png'
-  },
-
-  {
-    id: 2,
-    descricao: 'borracha',
-    quantidadeDesejada: 4,
-    urlFoto: '/assets/imagens/borracha.png'
-  },
-
-  {
-    id: 1,
-    descricao: 'Caixa de lápis',
-    quantidadeDesejada: 2,
-    urlFoto: '/assets/imagens/caixa-de-lapis.png'
-  },
-]
+  material = new Material();
+  materialPesquisa = new Material();
+materiais: Array<Material>;
   constructor() { }
 
   ngOnInit(): void {
+    this.materiais = [
+      {
+        id: 1,
+        descricao: 'apontador',
+        quantidadeDesejada: 2,
+        urlFoto: '/assets/imagens/apontador.png'
+      },
+    
+      {
+        id: 2,
+        descricao: 'borracha',
+        quantidadeDesejada: 4,
+        urlFoto: '/assets/imagens/borracha.png'
+      },
+    
+      {
+        id: 3,
+        descricao: 'Caixa de lápis',
+        quantidadeDesejada: 2,
+        urlFoto: '/assets/imagens/caixa-de-lapis.png'
+      },
+    ]
   }
   
   excluirMaterial(){
 
   }
 
-  editarMaterial(){
+  editarMaterial(item: Material){
+    this.material = item;
+  }
+
+  todos(){
+    this.materiais = [
+      {
+        id: 1,
+        descricao: 'apontador',
+        quantidadeDesejada: 2,
+        urlFoto: '/assets/imagens/apontador.png'
+      },
     
+      {
+        id: 2,
+        descricao: 'borracha',
+        quantidadeDesejada: 4,
+        urlFoto: '/assets/imagens/borracha.png'
+      },
+    
+      {
+        id: 3,
+        descricao: 'Caixa de lápis',
+        quantidadeDesejada: 2,
+        urlFoto: '/assets/imagens/caixa-de-lapis.png'
+      },
+    ]
+  }
+
+  pesquisarMaterial(){
+    console.log(this.materialPesquisa)
+    this.materiais = this.materiais.filter(m => m.id === this.materialPesquisa.id);
+    console.log(this.materiais);
   }
 }
